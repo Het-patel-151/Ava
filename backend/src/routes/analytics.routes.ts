06a9c52aaac176b937acb8443ac89050b8197a10
Het-patel-151 <het369p@gmail.com>
2026-06-21 22:00:33 +0530

    Delete backend directory

--- backend/src/routes/analytics.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import { getAnalytics } from '../controllers/analytics.controller';
-import { authenticate } from '../middleware/auth.middleware';
-
-const router = Router();
-router.use(authenticate);
-router.get('/', (req: Request, res: Response, next: NextFunction) => getAnalytics(req, res, next));
-export default router;
